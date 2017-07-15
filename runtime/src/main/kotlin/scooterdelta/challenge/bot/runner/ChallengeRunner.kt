ae871554aca36858dae7f00e4684c64973c8341d
Anthony Farquharson <scooterdelta@gmail.com>
2017-07-15 11:23:21 +0200

    Private property

--- runtime/src/main/kotlin/scooterdelta/challenge/bot/runner/ChallengeRunner.kt
+++ runtime/src/main/kotlin/scooterdelta/challenge/bot/runner/ChallengeRunner.kt
@@ -3,7 +3,7 @@ package scooterdelta.challenge.bot.runner
 import scooterdelta.challenge.bot.process.ProcessEngine
 import javax.inject.Inject
 
-class ChallengeRunner @Inject constructor(val processEngine: ProcessEngine) : Runnable {
+class ChallengeRunner @Inject constructor(private val processEngine: ProcessEngine) : Runnable {
 
     override fun run() {
         processEngine.run()
