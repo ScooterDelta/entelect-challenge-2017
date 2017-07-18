3ac65ef06f455c91a852fb19c0be28b0648701e9
Anthony Farquharson <anthonyf2@discovery.co.za>
2017-07-18 15:47:33 +0200

    Adding additional attack codes.

--- common/src/main/kotlin/scooterdelta/challenge/bot/common/lookup/Code.kt
+++ common/src/main/kotlin/scooterdelta/challenge/bot/common/lookup/Code.kt
@@ -2,5 +2,11 @@ package scooterdelta.challenge.bot.common.lookup
 
 enum class Code(val value: Int) {
     DO_NOTHING(0),
-    FIRESHOT(1);
+    FIRE_SHOT(1),
+    FIRE_DOUBLE_SHOT_VERTICAL(2),
+    FIRE_DOUBLE_SHOT_HORIZONTAL(3),
+    FIRE_CORNER_SHOT(4),
+    FIRE_CROSS_SHOT_DIAGONAL(5),
+    FIRE_CROSS_SHOT_HORIZONTAL(6),
+    FIRE_SEEKER_MISSILE(7);
 }
