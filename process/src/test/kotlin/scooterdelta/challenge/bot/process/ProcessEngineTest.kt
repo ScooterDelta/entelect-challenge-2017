aa13cd746f0f85eba59a2b5956692851e0c7abe3
Anthony Farquharson <scooterdelta@gmail.com>
2017-07-19 01:00:44 +0200

    Fixing tests

--- process/src/test/kotlin/scooterdelta/challenge/bot/process/ProcessEngineTest.kt
+++ process/src/test/kotlin/scooterdelta/challenge/bot/process/ProcessEngineTest.kt
@@ -8,7 +8,11 @@ import org.junit.Before
 import org.junit.Test
 import scooterdelta.challenge.bot.common.state.local.FileState
 import scooterdelta.challenge.bot.process.converter.GameStateDeserializer
+import scooterdelta.challenge.bot.process.processes.attack.BuildHuntDestroyProbabilityMapProcess
 import scooterdelta.challenge.bot.process.processes.attack.BuildProbabilityMapProcess
+import scooterdelta.challenge.bot.process.processes.attack.SelectAttackCommandProcess
+import scooterdelta.challenge.bot.process.processes.attack.SpecialWeaponProbabilityMapProcess
+import scooterdelta.challenge.bot.process.processes.attack.special.*
 import scooterdelta.challenge.bot.process.processes.placement.RandomPlacementImpl
 import java.io.File
 import java.util.*
@@ -18,18 +22,44 @@ class ProcessEngineTest {
     val PLAYER_KEY: String = "B"
     lateinit var processEngine: ProcessEngine
 
-    @Before
-    fun setUp() {
-        val workingDir: File = File(Resources.getResource("data/").toURI())
-        processEngine = ProcessEngine(
-                FileState(workingDir, PLAYER_KEY),
-                GameStateDeserializer(ObjectMapper()),
-                arrayListOf(RandomPlacementImpl(Random())),
-                arrayListOf(BuildProbabilityMapProcess()))
+    @Test
+    fun runProcessEngineAttack() {
+        val workingDir: File = File(Resources.getResource("data/run").toURI())
+        processEngine = buildProcessEngine(workingDir)
+        processEngine.run()
     }
 
     @Test
     fun runProcessEngine() {
+        val workingDir: File = File(Resources.getResource("data/start").toURI())
+        processEngine = buildProcessEngine(workingDir)
         processEngine.run()
+    }
+
+    private fun buildProcessEngine(workingDir: File): ProcessEngine {
+        return ProcessEngine(
+                FileState(workingDir, PLAYER_KEY),
+                GameStateDeserializer(ObjectMapper()),
+                arrayListOf(
+                        RandomPlacementImpl(Random())
+                ),
+                arrayListOf(
+                        BuildProbabilityMapProcess(),
+                        SpecialWeaponProbabilityMapProcess(arrayListOf(
+                                DoubleShotVerticalCalculator(),
+                                DoubleShotHorizontalCalculator(),
+                                CornerShotCalculator(),
+                                CrossDiagonalShotCalculator(),
+                                CrossHorizontalShotCalculator(),
+                                SeekerMissileCalculator()
+                        )),
+
+                        // RUN HUNT DESTROY LAST - Overrides previous probabilities while destroying ship
+                        BuildHuntDestroyProbabilityMapProcess(),
+
+                        // Select most likely weapon/location choice
+                        SelectAttackCommandProcess()
+                )
+        )
     }
 }
