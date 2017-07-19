87541b5c9dcb5d2397f4c09b37e87e70a0e1d6c1
Anthony Farquharson <anthonyf2@discovery.co.za>
2017-07-19 14:41:14 +0200

    Adding log aggregation python script (to make life easier)

---
+++ scripts/aggregate-logs.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+
+def main():
+    check_program_args()
+
+    working_dir = sys.argv[1]
+    target_file = sys.argv[2]
+    write_file = sys.argv[3]
+    folders = next(os.walk(working_dir))[1]
+
+    with open(working_dir + '\\' + write_file, 'w') as outfile:
+        for folder in folders:
+            file_with_path = working_dir + '\\' + folder + '\\' + target_file
+            if os.path.isfile(file_with_path):
+                with(open(file_with_path)) as infile:
+                    for line in infile:
+                        outfile.write(line)
+
+
+def check_program_args():
+    assert len(sys.argv) == 4
+
+
+if __name__ == '__main__':
+    main()
