fb9fc19adfa43f1e8d6827a61ce2252b31f76b0b
Anthony Farquharson <anthonyf2@discovery.co.za>
2017-07-19 15:20:22 +0200

    Fixing aggregate logs functions

--- scripts/aggregate-logs.py
+++ scripts/aggregate-logs.py
@@ -10,13 +10,14 @@ def main():
     write_file = sys.argv[3]
     folders = next(os.walk(working_dir))[1]
 
-    with open(working_dir + '\\' + write_file, 'w') as outfile:
+    with open(os.path.join(working_dir, write_file), 'w') as outfile:
         for folder in folders:
-            file_with_path = working_dir + '\\' + folder + '\\' + target_file
+            file_with_path = os.path.join(working_dir, folder, target_file)
             if os.path.isfile(file_with_path):
                 with(open(file_with_path)) as infile:
                     for line in infile:
                         outfile.write(line)
+                    outfile.write(os.linesep)
 
 
 def check_program_args():
